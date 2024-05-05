37e29d18b8d2d8a3500957020576ed85f5abec32
yasmensarhan27 <yasmensarhan27@gmail.com>
2024-05-05 17:15:31 +0000

    edit unittest

--- final_slow_version/unittest_slowversion.py
+++ final_slow_version/unittest_slowversion.py
@@ -1,24 +1,22 @@
 import unittest
 from unittest.mock import MagicMock
+import numpy as np
 from fastjet_slow_version import *
 
 
 class TestSlowJetAlgorithm(unittest.TestCase):
     def setUp(self):
         self.input_particles = [
-            MagicMock(rapidity=lambda: 1, phi=lambda: 0, pt=lambda: 10),
-            MagicMock(rapidity=lambda: 2, phi=lambda: 1, pt=lambda: 20),
-            MagicMock(rapidity=lambda: 3, phi=lambda: 2, pt=lambda: 30)
+            MagicMock(rapidity=lambda: 1, phi=lambda: 0, pt2=lambda: 10),
+            MagicMock(rapidity=lambda: 2, phi=lambda: 1, pt2=lambda: 20),
+            MagicMock(rapidity=lambda: 3, phi=lambda: 2, pt2=lambda: 30)
         ]
         self.R = 0.7
 
     def test_phi_mpi_pi(self):
-        self.assertAlmostEqual(phi_mpi_pi(3*np.pi), -np.pi)
-        self.assertAlmostEqual(phi_mpi_pi(5*np.pi), np.pi)
-
-    def test_SlowJetAlgorithm_init(self):
-        slow_jet_algo = SlowJetAlgorithm(self.input_particles)
-        self.assertEqual(slow_jet_algo.particles, self.input_particles)
+        phi = -3 * np.pi
+        adjusted_phi = phi_mpi_pi(phi)
+        self.assertAlmostEqual(np.abs(adjusted_phi), np.abs(np.pi))
 
     def test_cluster(self):
         slow_jet_algo = SlowJetAlgorithm(self.input_particles)
