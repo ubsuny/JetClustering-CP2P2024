cfc1b350d21fde1c58bd0480787ac2edbd72f11b
yasmensarhan27 <yasmensarhan27@gmail.com>
2024-05-05 13:30:03 +0000

    add docstring and comments

--- final slow_version/fastjet_slow_version.py
+++ final slow_version/fastjet_slow_version.py
@@ -3,55 +3,100 @@ from input_module import *
 ##-----------------------Slow version--------------------------##
 # Define phi_mpi_pi function to handle the periodicity of the azimuthal angle
 def phi_mpi_pi(phi):
+    """
+    Adjusts the azimuthal angle to lie in the range [-pi, pi].
+
+    Args:
+        phi (float): Azimuthal angle in radians.
+
+    Returns:
+        float: Adjusted azimuthal angle.
+    """
+    # Adjust phi to be within [-pi, pi] range
     while phi < -np.pi:
-        phi += 2*np.pi
+        phi += 2 * np.pi
     while phi > np.pi:
-        phi -= 2*np.pi
+        phi -= 2 * np.pi
     return phi
 
 class SlowJetAlgorithm:
+    """
+    Implements a slow version of the jet clustering algorithm.
+
+    Attributes:
+        particles (list): List of particles for clustering.
+    """
+
     def __init__(self, particles):
+        """
+        Initializes the SlowJetAlgorithm with particles.
+
+        Args:
+            particles (list): List of particles for clustering.
+        """
         self.particles = particles
 
     def cluster(self, R):
+        """
+        Clusters particles into jets using a slow algorithm.
+
+        Args:
+            R (float): Jet resolution parameter.
+
+        Returns:
+            list: List of clustered jets.
+        """
         jets = []
         my_particles = self.particles
-        num_particles=len(my_particles)
 
-        while(num_particles!=0):
-            num_particles=len(my_particles)
-            dij = dRij2 = np.zeros((num_particles,num_particles))
+        # Cluster until no particles left
+        while len(my_particles) != 0:
+            num_particles = len(my_particles)
+            dij = dRij2 = np.zeros((num_particles, num_particles))
             diB = np.zeros(num_particles)
 
-            for i,pi in enumerate(my_particles):
-                for j,pj in enumerate(my_particles):
-                    dRij2[i,j] = (pi.rapidity() - pj.rapidity())**2 + phi_mpi_pi(pi.phi() - pj.phi())**2
-                    dij[i,j]  = min(pi.pt2(), pj.pt2()) * ((dRij2[i,j]))/(R**2)
+            # Calculate pairwise distances and minimum pt
+            for i, pi in enumerate(my_particles):
+                for j, pj in enumerate(my_particles):
+                    # Calculate distance squared between particles
+                    dRij2[i, j] = (pi.rapidity() - pj.rapidity())**2 + phi_mpi_pi(pi.phi() - pj.phi())**2
+                    # Calculate dij
+                    dij[i, j] = min(pi.pt2(), pj.pt2()) * ((dRij2[i, j])) / (R**2)
 
+                # Calculate diB
                 diB[i] = pi.pt2()
 
-            if len(np.nonzero(dij)[0])!=0:
+            # If any non-zero elements in dij
+            if len(np.nonzero(dij)[0]) != 0:
+                # Check if minimum pt less than minimum dij
                 if np.min(diB) < np.min(dij[np.nonzero(dij)]):
+                    # Get index of minimum pt
                     dmin = np.min(diB[np.nonzero(diB)])
                     idx_i = np.where(diB == dmin)[0][0]
+                    # Add particle to jets and remove from my_particles
                     jets.append(my_particles[idx_i])
                     my_particles.pop(idx_i)
-
                 else:
+                    # Get indices of minimum dij
                     dmin = np.min(dij[np.nonzero(dij)])
                     idx_i, idx_j = np.where(dij == dmin)[0]
+                    # Combine particles and update my_particles
                     combined_particle = my_particles[idx_i] + my_particles[idx_j]
-                    my_particles = list(np.delete(np.array(my_particles),(idx_i,idx_j)))
+                    my_particles = list(np.delete(np.array(my_particles), (idx_i, idx_j)))
                     my_particles.append(combined_particle)
+            # If all elements in dij are zero
             else:
+                # Get index of minimum pt
                 dmin = np.min(diB[np.nonzero(diB)])
                 idx_i = np.where(diB == dmin)[0][0]
+                # Add particle to jets and remove from my_particles
                 jets.append(my_particles[idx_i])
                 my_particles.pop(idx_i)
                 break
 
         return jets
 
+# Usage example
 slow_jet_algo = SlowJetAlgorithm(input_particles)
 start_time = time.time()
 slow_jets = sorted(slow_jet_algo.cluster(R), key=lambda jet: -jet.pt())
